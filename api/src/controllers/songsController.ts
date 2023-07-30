36ddc0e8ae926caddb83a423b51b9b1f66573ce1
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-07-30 09:49:34 -0300

    peticion de 10 canciones mas elegidas

--- api/src/controllers/songsController.ts
+++ api/src/controllers/songsController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express'
 
 
   export async function getSong(req:Request, res:Response){
+    console.log("esta ejecutando getSongs")
     try {
       const { id } = req.params;
       const song = await Song.findById(id);
@@ -53,6 +54,15 @@ import { Request, Response } from 'express'
         res.json(tittles)
       } catch (error:any) {
         res.json({message: error.message})
+      }
+    }
+    export async function getSongByFavourite(_req: Request, res: Response) {
+      try {
+        const top = await Song.find({favourite:{$gte : 1 }}).sort({ favourite: -1 }).limit(10)
+        console.log(top.length)
+        res.status(200).json(top)
+      } catch (error:any) {
+        res.send(error.message)
       }
     }
 
--- api/src/routes/songRoute.ts
+++ api/src/routes/songRoute.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
-import { getSong, getSongs, getSongsByTittle, getSongsByAlbum, getSongsByProducer, getSongTittles } from '../controllers/songsController'
+import { getSong, getSongs, getSongsByTittle, getSongsByAlbum, getSongsByProducer, getSongTittles, getSongByFavourite } from '../controllers/songsController'
 
 const router = Router()
 
+router.get('/top', getSongByFavourite);
 router.get('/', getSongs);
 router.get('/:id', getSong);
-router.get('/tittle/:tittle', getSongsByTittle)
-router.get('/album/:album', getSongsByAlbum)
-router.get('/producer/:producer', getSongsByProducer)
-router.get('/all/tittles', getSongTittles)
+router.get('/tittle/:tittle', getSongsByTittle);
+router.get('/album/:album', getSongsByAlbum);
+router.get('/producer/:producer', getSongsByProducer);
+router.get('/all/tittles', getSongTittles);
+
 
 
 export default router
